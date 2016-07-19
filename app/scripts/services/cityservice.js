f71f4ea865261998249a16ffcb53902b6cb18151
rdelvallej32 <rdelvallej@gmail.com>
2016-07-19 14:03:15 -0400

    Generate custom service to use for city name
    - Will use to inject service in both home and forecast views

---
+++ app/scripts/services/cityservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name weatherAppApp.cityService
+ * @description
+ * # cityService
+ * Service in the weatherAppApp.
+ */
+angular.module('weatherAppApp')
+  .service('cityService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
