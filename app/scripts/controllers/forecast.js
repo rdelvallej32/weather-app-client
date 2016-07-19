7e2d7f7733199a1cc2f8896117c74dceead57b4b
rdelvallej32 <rdelvallej@gmail.com>
2016-07-19 14:29:56 -0400

    Inject scope and city services into controller
    - scope service will watch the name of the city name and change
    the cityservice city property depending on user input

--- app/scripts/controllers/forecast.js
+++ app/scripts/controllers/forecast.js
@@ -8,10 +8,17 @@
  * Controller of the weatherAppApp
  */
 angular.module('weatherAppApp')
-  .controller('ForecastCtrl', function () {
+  .controller('ForecastCtrl', ['cityService', '$scope', function (cityService, $scope) {
+
+    $scope.city = cityService.city;
+
+    $scope.$watch('city', function() {
+      cityService.city = $scope.city;
+    });
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+  }]);
