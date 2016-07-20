3197ef9115f33aee5ac6c057da66d30bcebfa7f3
rdelvallej32 <rdelvallej@gmail.com>
2016-07-20 10:38:14 -0400

    Add conversion methods for temp and date
    - Created two functions: One that converts the temp to F and one that converts
    the date into standard date format.

--- app/scripts/controllers/forecast.js
+++ app/scripts/controllers/forecast.js
@@ -18,8 +18,14 @@ angular.module('weatherAppApp')
 
     $scope.weatherResult = weatherApi.get({ APPID: '3967400fc266e26b0cb280c55cf69e22', q: $scope.city, cnt: 2});
 
-    console.log($scope.weatherResult);
-    
+    $scope.convertToF = function(degk) {
+      return Math.round((1.8 * (degk - 273)) + 32);
+    };
+
+    $scope.convertDt = function(dt) {
+      return new Date(dt * 1000);
+    };
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
