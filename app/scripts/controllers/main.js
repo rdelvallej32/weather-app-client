f6b74142d17508ac744310b17706e26f15321fef
rdelvallej32 <rdelvallej@gmail.com>
2016-07-20 14:33:18 -0400

    Add submit function to scope
    - Injected the location service to send the user to the forecast view
    on submit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,17 @@
  * Controller of the weatherAppApp
  */
 angular.module('weatherAppApp')
-  .controller('MainCtrl', ['cityService', '$scope', function (cityService, $scope) {
+  .controller('MainCtrl', ['cityService', '$scope', '$location', function (cityService, $scope, $location) {
 
     $scope.city = cityService.city;
 
     $scope.$watch('city', function() {
       cityService.city = $scope.city;
     });
+
+    $scope.submit = function() {
+      return $location.path('/forecast');
+    };
 
     this.awesomeThings = [
       'HTML5 Boilerplate',
