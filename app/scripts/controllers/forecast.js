8333339f1bc3bee4afa1548b0aafe7a221fc2fe6
rdelvallej32 <rdelvallej@gmail.com>
2016-07-20 09:55:55 -0400

    Add ngResource Get Request to WeatherAPI
    - Injected the weatherAPI factory and made call to openweather API

--- app/scripts/controllers/forecast.js
+++ app/scripts/controllers/forecast.js
@@ -8,7 +8,7 @@
  * Controller of the weatherAppApp
  */
 angular.module('weatherAppApp')
-  .controller('ForecastCtrl', ['cityService', '$scope', function (cityService, $scope) {
+  .controller('ForecastCtrl', ['cityService', 'weatherApi', '$scope', function (cityService, weatherApi, $scope) {
 
     $scope.city = cityService.city;
 
@@ -16,6 +16,10 @@ angular.module('weatherAppApp')
       cityService.city = $scope.city;
     });
 
+    $scope.weatherResult = weatherApi.get({ APPID: '3967400fc266e26b0cb280c55cf69e22', q: $scope.city, cnt: 2});
+
+    console.log($scope.weatherResult);
+    
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
